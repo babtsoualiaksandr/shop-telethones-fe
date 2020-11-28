66f795d761b3175c91e79d73fba0a51a0be224c6
Aliaksandr Babtsou <babtsoualiaksandr@gmail.com>
2020-11-28 18:58:12 +0300

    fieat: implementation 401 and 403

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://dhsvydcw18.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://1997fk87d0.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://g8hnk0zjyd.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://dhsvydcw18.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
